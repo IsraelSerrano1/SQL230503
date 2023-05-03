21c0a6f646340d066941343fec6251e549b798bf
IsraelSerrano1 <130450623+IsraelSerrano1@users.noreply.github.com>
2023-05-03 15:26:50 +0200

    Add files via upload

--- bibliobus.sql
+++ bibliobus.sql
@@ -3,39 +3,39 @@
 BEGIN;
 
 
-CREATE TABLE IF NOT EXISTS public.socio
-(
-    id_socio serial NOT NULL,
-    nombre character(25) NOT NULL,
-    edad integer,
-    PRIMARY KEY (id_socio)
-);
-
 CREATE TABLE IF NOT EXISTS public.libro
 (
     "ISBN" integer NOT NULL,
-    titulo character NOT NULL,
-    autor character NOT NULL,
+    titulo character(1) COLLATE pg_catalog."default" NOT NULL,
+    autor character(1) COLLATE pg_catalog."default" NOT NULL,
     n_ejemplares integer NOT NULL,
     id_tema integer NOT NULL,
-    PRIMARY KEY ("ISBN")
-);
-
-CREATE TABLE IF NOT EXISTS public.tema
-(
-    id_tema serial NOT NULL,
-    nombre character NOT NULL,
-    PRIMARY KEY (id_tema)
+    CONSTRAINT libro_pkey PRIMARY KEY ("ISBN")
 );
 
 CREATE TABLE IF NOT EXISTS public.libros_socios
 (
-    id_prestamo serial NOT NULL,
+    id_prestamo integer NOT NULL DEFAULT nextval('libros_socios_id_prestamo_seq'::regclass),
     fecha_prestamo date NOT NULL,
     fecha_devolucion date NOT NULL,
     id_socio integer NOT NULL,
     "ISBN" integer NOT NULL,
-    PRIMARY KEY (id_prestamo)
+    CONSTRAINT libros_socios_pkey PRIMARY KEY (id_prestamo)
+);
+
+CREATE TABLE IF NOT EXISTS public.socio
+(
+    id_socio integer NOT NULL DEFAULT nextval('socio_id_socio_seq'::regclass),
+    nombre character(25) COLLATE pg_catalog."default" NOT NULL,
+    edad integer,
+    CONSTRAINT socio_pkey PRIMARY KEY (id_socio)
+);
+
+CREATE TABLE IF NOT EXISTS public.tema
+(
+    id_tema integer NOT NULL DEFAULT nextval('tema_id_tema_seq'::regclass),
+    nombre character(15) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT tema_pkey PRIMARY KEY (id_tema)
 );
 
 ALTER TABLE IF EXISTS public.libro
@@ -47,16 +47,16 @@ ALTER TABLE IF EXISTS public.libro
 
 
 ALTER TABLE IF EXISTS public.libros_socios
-    ADD CONSTRAINT fk_socio_prestamo FOREIGN KEY (id_socio)
-    REFERENCES public.socio (id_socio) MATCH SIMPLE
+    ADD CONSTRAINT fk_libro_prestamo FOREIGN KEY ("ISBN")
+    REFERENCES public.libro ("ISBN") MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
 
 
 ALTER TABLE IF EXISTS public.libros_socios
-    ADD CONSTRAINT fk_libro_prestamo FOREIGN KEY ("ISBN")
-    REFERENCES public.libro ("ISBN") MATCH SIMPLE
+    ADD CONSTRAINT fk_socio_prestamo FOREIGN KEY (id_socio)
+    REFERENCES public.socio (id_socio) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID;
